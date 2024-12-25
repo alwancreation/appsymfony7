71f1e7debf214ab1c886758c04aa3bb0698d44d5
Hicham Ech-chaddady <hichamechchaddady@gmail.com>
2024-12-25 11:18:46 +0100

    version 1.0 security form

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-user',
+    description: 'Add a short description for your command',
+)]
+class CreateUserCommand extends Command
+{
+    protected static $defaultName = 'app:create-user';
+
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Creates a new user.')
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the user.')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password of the user.')
+            ->addArgument('roles', InputArgument::OPTIONAL, 'The roles of the user (comma-separated).', 'ROLE_USER');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $roles = explode(',', $input->getArgument('roles'));
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles($roles);
+
+        // Hash the password
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        // Persist the user
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $output->writeln('User successfully created!');
+
+        return Command::SUCCESS;
+    }
+}
