32b8d84cee3fb8dd9ecb87df288c1c6daccb5cea
Hicham Ech-chaddady <hichamechchaddady@gmail.com>
2024-12-29 00:27:47 +0100

    admin lte

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,7 +12,7 @@ class IndexController extends AbstractController
     #[Route(path: '/', name: 'app_home')]
     public function login(): Response
     {
-        return $this->render('index/index.html.twig', [
+        return $this->render('admin/index/index.html.twig', [
 
         ]);
     }
